cd8d7c0120eca65948203ed9f72cebb6b8ff9a2c
iseppid <sep.dimeglio@gmail.com>
2015-07-28 23:15:10 -0400

    OMO Code
    This

---
+++ levels.js
@@ -0,0 +1,25 @@
+Breakout.Colors = {
+
+  pastel: {
+    y: "grey",
+    p: "grey", 
+    b: "grey", 
+    g: "gold", 
+    o: "grey"  
+  },
+};
+
+Breakout.Levels = [
+  { colors: Breakout.Colors.pastel,
+    bricks: [
+      "", "", "", "", "", "",                      // spaces from top
+      "y     o   Y  o             Y",             // row 1
+      "   p                 p",                  // row 2
+      "       o    o               b",          // row 3
+      "                o        g",             // row 4 
+      "o   o  o  o       o   o            O"  // row 5
+    ]
+  },
+];
+
+
